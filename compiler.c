715f42c5c6ac1861c2e38ca31535dd064009ff78
Leo Rouleau <Leorouleau5070@gmail.com>
2026-02-07 13:03:36 -0500

    wrote identifierConstant

--- compiler.c
+++ compiler.c
@@ -144,6 +144,15 @@ static void declaration();
 static ParseRule* getRule(TokenType type);
 static void parsePrecedence(Precedence precedence);
 
+static uint8_t identifierConstant(Token* name) {
+     return makeConstant(OBJ_VAL(copyString(name->start, name->length)));
+}
+
+static uint8_t parseVariable(const char* errorMessage) {
+     consume(TOKEN_IDENTIFIER, errorMessage);
+     return identifierConstant(&parser.previous);
+}
+
 static void binary() {
      TokenType operatorType = parser.previous.type;
      ParseRule* rule = getRule(operatorType);
@@ -271,6 +280,19 @@ static void expression() {
      parsePrecedence(PREC_ASSIGNMENT);
 }
 
+static void varDeclaration() {
+     uint8_t global = parseVariable("Expect variable name.");
+
+     if (match(TOKEN_EQUAL)) {
+	  expression();
+     } else {
+	  emitByte(OP_NIL);
+     }
+     consume(TOKEN_SEMICOLON, "Expect ';' after variable declaration.");
+
+     defineVariable(global);
+}
+
 static void expressionStatement() {
      expression();
      consume(TOKEN_SEMICOLON, "Expect ';' after exppression.");
@@ -307,7 +329,11 @@ static void synchronize() {
 }
 
 static void declaration() {
-     statement();
+     if (match(TOKEN_VAR)) {
+	  varDeclaration();
+     } else {
+	  statement();
+     }
 
      if (parser.panicMode) synchronize();
 }
