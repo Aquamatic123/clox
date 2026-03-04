4c3de901caf850e556f26576eaf356cadb4e8d74
Leo Rouleau <Leorouleau5070@gmail.com>
2026-03-04 13:32:30 -0500

    26.2 done. Mark-Sweep Garbage Collection

--- memory.c
+++ memory.c
@@ -5,6 +5,11 @@
 #include "vm.h"
 #include "object.h"
 
+#ifdef DEBUG_LOG_GC
+#include <stdio.h>
+#include "debug.h"
+#endif
+
 void *reallocate(void *pointer, size_t oldSize, size_t newSize) {
      if (newSize > oldSize) {
 #ifdef DEBUG_STRESS_GC
@@ -23,6 +28,10 @@ void *reallocate(void *pointer, size_t oldSize, size_t newSize) {
 }
 
 static void freeObject(Obj* object) {
+#ifdef DEBUG_LOG_GC
+     printf("%p free type %d\n", (void*)object, object->type);
+#endif
+     
      switch (object->type) {
      case OBJ_CLOSURE: {
 	  FREE(ObjClosure, object);
@@ -62,5 +71,11 @@ void freeObjects() {
 }
 
 void collectGarbage() {
-     
+#ifdef DEBUG_LOG_GC
+     printf("-- gc begin\n");
+#endif
+
+#ifdef DEBUG_LOG_GC
+     printf("-- gc end\n");
+#endif
 }
