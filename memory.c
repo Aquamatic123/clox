1a8713bf313b019634dca9a428084a9d29e4b009
Leo Rouleau <Leorouleau5070@gmail.com>
2026-03-04 14:01:10 -0500

    26.3 done. Marking the Roots

--- memory.c
+++ memory.c
@@ -1,6 +1,7 @@
 #include "chunk.h"
 #include "stdlib.h"
 
+#include "compiler.h"
 #include "memory.h"
 #include "vm.h"
 #include "object.h"
@@ -91,7 +92,16 @@ static void markRoots() {
 	  markValue(*slot);
      }
 
+     for (int i = 0; i < vm.frameCount; i++) {
+	  markObject((Obj*)vm.frames[i].closure);
+     }
+
+     for (ObjUpvalue* upvalue = vm.openUpvalues; upvalue != NULL; upvalue = upvalue->next) {
+	  markObject((Obj*)upvalue);
+     }
+
      markTable(&vm.globals);
+     markCompilerRoots();
 }
 
 void collectGarbage() {
