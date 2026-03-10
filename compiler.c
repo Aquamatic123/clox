93c5c4fe4674f51c879d7a2e3f5cd1f665b42590
Leo Rouleau <Leorouleau5070@gmail.com>
2026-03-10 16:02:12 -0400

    28.4 done. Instance initializers.

--- compiler.c
+++ compiler.c
@@ -765,6 +765,9 @@ static void returnStatement() {
      if (match(TOKEN_SEMICOLON)) {
 	  emitReturn();
      } else {
+	  if (current->type == TYPE_INITIALIZER) {
+	       error("Can't return a value from an initializer.");
+	  }
 	  expression();
 	  consume(TOKEN_SEMICOLON, "Expect ';' after return variable.");
 	  emitByte(OP_RETURN);
